119f43cb335ae223ba2ff6562c688a9d4d3364fc
Madobe <sin_emerald@hotmail.com>
2018-10-07 14:03:41 -0600

    Set image alts and change selfie alt to be more descriptive

--- src/history/history.js
+++ src/history/history.js
@@ -14,31 +14,36 @@ class History extends Component {
         image: linkedInLogo,
         href: "https://www.linkedin.com/in/justin-choi-a8413955/",
         text: "Justin Choi",
+        alt: "LinkedIn Logo",
       },
       {
         image: githubLogo,
         href: "https://github.com/Makita",
         text: "Makita (Work)",
+        alt: "Github Logo",
       },
       {
         image: githubLogo,
         href: "https://github.com/Madobe",
         text: "Madobe (Personal)",
+        alt: "Github Logo",
       },
       {
         image: avatarImage,
         href: resumeRsc,
         text: "Resume (English)",
+        alt: "Avatar Icon",
       },
       {
         image: avatarImage,
         href: resumeJPRsc,
         text: "Resume (Japanese)",
+        alt: "Avatar Icon",
       },
     ];
     let links = values.map((hash) =>
       <div key={hash.text}>
-        <img src={hash.image} />
+        <img src={hash.image} alt={hash.alt} />
         <a href={hash.href}>{hash.text}</a>
       </div>
     );
@@ -46,7 +51,7 @@ class History extends Component {
     return (
       <div id="history">
         {links}
-        <img src={photo} alt="Photo" id="selfie" />
+        <img src={photo} alt="Selfie" id="selfie" />
       </div>
     );
   }
