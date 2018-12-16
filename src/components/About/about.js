d1911158422e8b470cbd04abbbbcf49d33627e5c
Madobe <sin_emerald@hotmail.com>
2018-12-15 21:08:38 -0700

    Fix wording

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -14,22 +14,22 @@ const About = () => (
     </blockquote>
     <p className={style.text}>
       I believe that cheesy quotes really embody the ideal of humanity and show
-      a wisdom that is all but lost as we compromise in life. As we move further
-      in life, words akin to "being realistic" become more prevalent. We begin
-      to limit ourselves by relying on these phrases, neglecting to continue to
+      a wisdom that is all but lost as we compromise in life. As we experience
+      more, words akin to "being realistic" become more prevalent. We begin to
+      limit ourselves by relying on these phrases, neglecting to continue to
       pursue ideals by stopping short.
     </p>
     <p className={style.text}>
       I recognize this inner conflict and strive to always chase the best
-      solution to the absolute limits of my ability. In my work, this means I
-      will make concessions where necessary but never discard a better solution
-      while the possibility exists, despite any hardship that may be associated
-      with it.
+      solution to the absolute limits of my ability. In my work, this means that
+      while I will make concessions where necessary, I will never discard a
+      better solution for so long as the possibility exists, despite any
+      difficulty that may be associated with it.
     </p>
     <p className={style.text}>
       I would like to bring this ideal to life with a company that endeavors to
-      surpass being just good and grow evermore. I know I still have much more
-      to grow and the environment for it is paramount. Please contact me if this
+      surpass being simply "good" and grow evermore. I know I still have much
+      more to grow and I would like to do it together. Please contact me if this
       is your company!
     </p>
   </div>
